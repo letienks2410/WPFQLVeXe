3c294f0f092fcbcbd64bc9a713c2fb5829860090
Hoangminhfcb <78634684+Hoangminhfcb@users.noreply.github.com>
2024-07-21 17:53:10 +0700

    nminh
    dat ve va thong tin ve

---
+++ WPFQLVeXe/View/NVDatVe.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using WPFQLVeXe.Models;
+using System;
+
+namespace WPFQLVeXe.View
+{
+    /// <summary>
+    /// Interaction logic for NVDatVe.xaml
+    /// </summary>
+    public partial class NVDatVe : UserControl
+    {
+        public NVDatVe()
+        {
+            InitializeComponent();
+        }
+
+        // load các table cho màn hình
+        BanVeEntities db = new BanVeEntities();
+
+        private ObservableCollection<TuyenXe> getTuyenXe()
+        {
+            return new ObservableCollection<TuyenXe>(db.TuyenXes.ToList());
+        }
+
+        private ObservableCollection<LichTrinh> getLichTrinh()
+        {
+            return new ObservableCollection<LichTrinh>(db.LichTrinhs.ToList());
+        }
+
+        private ObservableCollection<KhachHang> getKhachHang()
+        {
+            return new ObservableCollection<KhachHang>(db.KhachHangs.ToList());
+        }
+
+        // tự động tạo mã khách hàng
+        private string NewCustomerID()
+        {
+            string nextID;
+            string curID = db.KhachHangs.OrderByDescending(p => p.MaKh)
+                                        .Select(r => r.MaKh)
+                                        .First().ToString();
+            curID = curID.Remove(0, 2);
+            int intID = Convert.ToInt32(curID);
+            intID += 1;
+            nextID = "KH" + intID.ToString();
+            return nextID;
+        }
+
+        // tự động tạo mã vé mới
+        private string NewTicketID()
+        {
+            string nextID;
+            string curID = db.VeXes.OrderByDescending(p => p.MaVe)
+                                   .Select(r => r.MaVe)
+                                   .First().ToString();
+            curID = curID.Remove(0, 2);
+            int intID = Convert.ToInt32(curID);
+            intID += 1;
+            nextID = "V0" + intID.ToString();
+            return nextID;
+        }
+
+        // check textbox null
+        private bool isNull()
+        {
+            if (!String.IsNullOrEmpty(tbTenKH.Text)
+                && !String.IsNullOrEmpty(tbCMND.Text)
+                && !String.IsNullOrEmpty(tbDiaChi.Text)
+                && !String.IsNullOrEmpty(tbGiaVe.Text)
+                && !String.IsNullOrEmpty(tbSDT.Text))
+                return false;
+            return true;
+        }
+
+        private void ClearAll()
+        {
+            cbTuyenDi.Text = "";
+            cbNgayDi.Text = "";
+            cbGhe.Text = "";
+            tbGioDi.Clear();
+            tbGioDen.Clear();
+            tbGiaVe.Clear();
+            tbDiemDen.Clear();
+            tbDiaChi.Clear();
+            tbCMND.Clear();
+            tbMaTuyen.Clear();
+            tbNgayDen.Clear();
+            tbSDT.Clear();
+            tbSoXe.Clear();
+            tbTenKH.Clear();
+        }
+
+        // tạo khách hàng mới
+        private void CreateCustomer(string id)
+        {
+            if (isNull())
+            {
+                MessageBox.Show("Không được để trống thông tin khách hàng!", "Thông báo!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                KhachHang _new = new KhachHang();
+                _new.MaKh = id;
+                _new.TenKh = tbTenKH.Text;
+                _new.DiaChi = tbDiaChi.Text;
+                _new.Cmnd = tbCMND.Text;
+                _new.Sdt = tbSDT.Text;
+
+                db.KhachHangs.Add(_new);
+                db.SaveChanges();
+            }
+        }
+
+        // load db tuyến đi
+        private void LoadedDatVe(object sender, RoutedEventArgs e)
+        {
+            cbTuyenDi.ItemsSource = getTuyenXe();
+            cbTuyenDi.DisplayMemberPath = "DiaDiemDi";
+        }
+
+        // combobox tuyến đi thay đổi
+        private void cbTuyenDi_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = cbTuyenDi.SelectedItem as TuyenXe;
+            if (selected != null)
+            {
+                // chọn lịch trình của tuyến đi
+                var query = from b in db.LichTrinhs
+                            where b.MaTuyen == selected.MaTuyen
+                            select b;
+
+                // tạo danh sách để binding cho combobox ngày đi
+                ObservableCollection<LichTrinh> data = new ObservableCollection<LichTrinh>(query.ToList());
+                cbNgayDi.ItemsSource = data;
+                cbNgayDi.DisplayMemberPath = "NgayDi";
+            }
+        }
+
+        // Load danh sách vé chưa đăng kí
+        private void btnLoad(object sender, RoutedEventArgs e)
+        {
+            // tạo ghế
+            List<string> _ghe = new List<string>();
+            for (int i = 0; i < 20; i++)
+            {
+                if (i < 9)
+                    _ghe.Add("GH0" + (i + 1).ToString());
+                else
+                    _ghe.Add("GH" + (i + 1).ToString());
+            }
+
+            // chọn số xe 
+            var query = from b in db.VeXes
+                        where b.SoXe == tbSoXe.Text
+                        select b;
+
+            // xóa ghế đã có trong xe
+            foreach (VeXe i in query)
+                _ghe.Remove(i.MaGhe);
+
+            // binding ghế lên combobox
+            cbGhe.ItemsSource = _ghe.ToList();
+        }
+
+        // nút thanh toán
+        private void btnThanhToan(object sender, RoutedEventArgs e)
+        {
+            if (isNull())
+                MessageBox.Show("Không được để trống textbox nào!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+            else
+            {
+                string _newCustomerID = NewCustomerID();
+
+                VeXe _newTicket = new VeXe();
+                TuyenXe _tuyenxe = cbTuyenDi.SelectedItem as TuyenXe;
+                LichTrinh _lichtrinh = cbNgayDi.SelectedItem as LichTrinh;
+
+                // show thông báo xác nhận
+                var Confirm = MessageBox.Show("Thanh toán với số tiền là: " + tbGiaVe.Text, "Xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (Confirm == MessageBoxResult.Yes)
+                {
+                    CreateCustomer(_newCustomerID); // tạo khách hàng mới
+
+                    var _newTickerID = NewTicketID();
+                    // nếu id đã tồn tại thì tăng lên
+                    while (db.VeXes.Find(_newTickerID) != null)
+                    {
+                        _newTickerID += 1;
+                    }
+                    _newTicket.MaVe = _newTickerID;
+                    _newTicket.MaKh = _newCustomerID;
+                    _newTicket.MaGhe = cbGhe.SelectedItem.ToString();
+                    _newTicket.GioDi = _tuyenxe.GioDi;
+                    _newTicket.GiaVe = Convert.ToInt32(tbGiaVe.Text);
+                    _newTicket.SoXe = tbSoXe.Text;
+                    _newTicket.NgayDi = _lichtrinh.NgayDi;
+                    _newTicket.NhanVienBv = "NV001";
+
+                    db.VeXes.Add(_newTicket); // tạo vé mới
+                    db.SaveChanges();
+
+                    MessageBox.Show("Đã thêm vé vào dữ liệu", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    ClearAll();
+                }
+                else
+                {
+                    MessageBox.Show("Đã hủy thanh toán", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    ClearAll();
+                }
+            }
+        }
+    }
+}
---
+++ WPFQLVeXe/View/NVThongTinDatVe.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using WPFQLVeXe.Models;
+using System.Windows.Input;
+
+namespace WPFQLVeXe.View
+{
+    /// <summary>
+    /// Interaction logic for NVThongTinDatVe.xaml
+    /// </summary>
+    public partial class NVThongTinDatVe : UserControl
+    {
+        public NVThongTinDatVe()
+        {
+            InitializeComponent();
+        }
+        // Data Context
+        BanVeEntities db = new BanVeEntities();
+        
+        private ObservableCollection<VeXe> getTickets()
+        {
+            return new ObservableCollection<VeXe>(db.VeXes.ToList());
+        }
+
+        // set tb khách hàng enable
+        private void setEnable()
+        {
+            tbTen.IsReadOnly = false;
+            tbCMND.IsReadOnly = false;
+            tbDiaChi.IsReadOnly = false;
+            tbSDT.IsReadOnly = false;
+        }
+
+        private void setMutable()
+        {
+            tbTen.IsReadOnly = true;
+            tbCMND.IsReadOnly = true;
+            tbDiaChi.IsReadOnly = true;
+            tbSDT.IsReadOnly = true;
+        }
+
+        private void Search()
+        {
+            var search = from b in db.VeXes
+                        where b.MaVe.Contains(tbSearch.Text)
+                        select b;
+            ObservableCollection<VeXe> data = new ObservableCollection<VeXe>(search.ToList());
+            dtgVe.ItemsSource = data;
+        }
+
+        // nút search
+        private void btnSearch(object sender, RoutedEventArgs e)
+        {
+            Search();
+        }
+
+        private void EnterKey(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                Search();
+                e.Handled = true;
+            }
+        }
+
+        // load data vé
+        private void dtgVeLoaded(object sender, RoutedEventArgs e)
+        {
+            dtgVe.ItemsSource = getTickets();
+        }
+
+        // chọn vé
+        private void dtgVe_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dtgVe.SelectedItem != null)
+            {
+                // khởi tạo các biến
+                VeXe _ticket = dtgVe.SelectedItem as VeXe;
+                KhachHang _customer = db.KhachHangs.Find(_ticket.MaKh);
+                LichTrinh _lichtrinh = db.LichTrinhs.Where(s => s.SoXe == _ticket.SoXe).SingleOrDefault();
+                TuyenXe _tuyenxe = db.TuyenXes.Where(s => s.MaTuyen == _lichtrinh.MaTuyen).SingleOrDefault();
+
+                // binding các textbox khách hàng
+                tbTen.Text = _customer.TenKh;
+                tbCMND.Text = _customer.Cmnd;
+                tbDiaChi.Text = _customer.DiaChi;
+                tbSDT.Text = _customer.Sdt;
+
+                // binding các textbox thông tin
+                tbGhe.Text = _ticket.MaGhe;
+                tbGioDen.Text = _tuyenxe.GioDen.ToString();
+                tbGioDi.Text = _tuyenxe.GioDi.ToString();
+                tbMaTuyen.Text = _tuyenxe.MaTuyen;
+                tbNgayDen.Text = _lichtrinh.NgayDen.ToShortDateString();
+                tbNgayDi.Text = _lichtrinh.NgayDi.ToShortDateString();
+                tbTuyenDen.Text = _tuyenxe.DiaDiemDen;
+                tbTuyenDi.Text = _tuyenxe.DiaDiemDi;
+            }
+        }
+
+        private void btnUpdate(object sender, RoutedEventArgs e)
+        {
+            if (dtgVe.SelectedItem == null)
+                MessageBox.Show("Có chọn gì đâu mà sửa", "Thông báo", MessageBoxButton.OK, MessageBoxImage.None);
+            else
+            {
+                var question = MessageBox.Show("Bạn muốn thay đổi thông tin khách hàng?", "Xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (question == MessageBoxResult.Yes)
+                {
+                    setEnable();
+                }
+            }
+        }
+
+        private void btnSave(object sender, RoutedEventArgs e)
+        {
+            if (dtgVe.SelectedItem == null)
+                MessageBox.Show("Có chọn gì đâu mà lưu", "Thông báo", MessageBoxButton.OK, MessageBoxImage.None);
+            else
+            {
+                setMutable();
+                var selected = dtgVe.SelectedItem as VeXe;
+
+                var question = MessageBox.Show("Lưu thông tin khách hàng?", "Xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (question == MessageBoxResult.Yes)
+                {
+                    KhachHang _customer = db.KhachHangs.Find(selected.MaKh);
+
+                    _customer.TenKh = tbTen.Text;
+                    _customer.Cmnd = tbCMND.Text;
+                    _customer.DiaChi = tbDiaChi.Text;
+                    _customer.Sdt = tbSDT.Text;
+
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        private void btnClear(object sender, RoutedEventArgs e)
+        {
+            tbSearch.Clear();
+            dtgVe.ItemsSource = getTickets();
+        }
+    }
+}
